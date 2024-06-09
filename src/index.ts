721c6f6e931d7efbbde14cea0c9f4f886936074b
Rachel Grimwood <85718875+regrimwood@users.noreply.github.com>
2024-06-09 16:24:01 +1200

    add mssql connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-import fastify from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 import { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts";
+import mssql from "fastify-mssql";
 
 const server = fastify().withTypeProvider<JsonSchemaToTsProvider>();
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
-});
+async function dbConnect(server: FastifyInstance) {
+  server.register(mssql, {
+    server: "localhost",
+    port: 1433,
+    user: "root",
+    password: "root",
+    database: "iticket",
+  });
+
+  console.log("Connected to database");
+}
+
+server.register(dbConnect);
+
+async function apiRoutes(server: FastifyInstance) {
+  server.get("/ping", async (request, reply) => {
+    return "pong\n";
+  });
+}
+
+server.register(apiRoutes, { prefix: "/api" });
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
