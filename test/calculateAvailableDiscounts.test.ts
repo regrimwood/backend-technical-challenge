82e68152d6b079f5012dc6ffd01592664e02a40c
Rachel Grimwood <85718875+regrimwood@users.noreply.github.com>
2024-06-18 09:18:01 +1200

    improve test

--- test/calculateAvailableDiscounts.test.ts
+++ test/calculateAvailableDiscounts.test.ts
@@ -1,37 +1,17 @@
 import calculateAvailableDiscounts from "../src/utils/calculateAvailableDiscounts";
+import { DiscountType } from "../src/utils/types/discountType";
 
 describe("calculateAvailableDiscounts", () => {
   it("should return valid discounts", () => {
     const priceQuantities = new Map<number, number>([
       [1, 5],
-      [2, 10],
+      [2, 5],
       [3, 15],
     ]);
 
-    const discounts = [
+    const discounts: DiscountType = [
       {
-        discount_id: 1,
-        name: "Discount 1",
-        percentDiscount: 10,
-        fixedPrice: undefined,
-        price_id: 1,
-        min_quantity: 5,
-        max_quantity: 10,
-      },
-      {
-        discount_id: 2,
-        name: "Discount 2",
-        percentDiscount: 20,
-        fixedPrice: undefined,
-        price_id: 2,
-        min_quantity: 10,
-        max_quantity: 15,
-      },
-    ];
-
-    const expectedDiscounts = [
-      {
-        discountId: 1,
+        id: 1,
         name: "Discount 1",
         percentDiscount: 10,
         fixedPrice: undefined,
@@ -44,7 +24,7 @@ describe("calculateAvailableDiscounts", () => {
         ],
       },
       {
-        discountId: 2,
+        id: 2,
         name: "Discount 2",
         percentDiscount: 20,
         fixedPrice: undefined,
@@ -53,6 +33,76 @@ describe("calculateAvailableDiscounts", () => {
             priceId: 2,
             minQuantity: 10,
             maxQuantity: 15,
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Discount 3",
+        percentDiscount: 20,
+        fixedPrice: undefined,
+        items: [
+          {
+            priceId: 2,
+            minQuantity: 10,
+            maxQuantity: 15,
+          },
+          {
+            priceId: 3,
+            minQuantity: 10,
+            maxQuantity: undefined,
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Discount 4",
+        percentDiscount: 20,
+        fixedPrice: undefined,
+        items: [
+          {
+            priceId: 2,
+            minQuantity: 5,
+            maxQuantity: 15,
+          },
+          {
+            priceId: 3,
+            minQuantity: 5,
+            maxQuantity: undefined,
+          },
+        ],
+      },
+    ];
+
+    const expectedDiscounts: DiscountType = [
+      {
+        id: 1,
+        name: "Discount 1",
+        percentDiscount: 10,
+        fixedPrice: undefined,
+        items: [
+          {
+            priceId: 1,
+            minQuantity: 5,
+            maxQuantity: 10,
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Discount 4",
+        percentDiscount: 20,
+        fixedPrice: undefined,
+        items: [
+          {
+            priceId: 2,
+            minQuantity: 5,
+            maxQuantity: 15,
+          },
+          {
+            priceId: 3,
+            minQuantity: 5,
+            maxQuantity: undefined,
           },
         ],
       },
